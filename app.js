7355899d7e3a995d44f1a6a20488e0cf94a1cde6
Ngan Khanh Nguyen T <ngankhanh98@gmail.com>
2018-11-15 09:57:32 +0700

    Material-design-icons added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var signinRouter = require('./routes/signin');
+var postRouter = require('./routes/post');
 
 var app = express();
 
@@ -21,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/', indexRouter);
 app.use('/signin', signinRouter);
-
+app.use('/post/', postRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
